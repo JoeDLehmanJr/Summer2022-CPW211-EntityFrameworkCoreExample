ea658a706d901638b2d9058b7d67a861b6c4a915
JoeLehmanJr <joejr65@live.com>
2022-07-20 19:05:11 -0700

    Added EmailAddress to student class and updated DB with migration.

---
+++ EntityFrameworkCoreExample/Migrations/20220721015917_AddedStudentEmail.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EntityFrameworkCoreExample.Migrations
+{
+    public partial class AddedStudentEmail : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "EmailAddress",
+                table: "Students",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "EmailAddress",
+                table: "Students");
+        }
+    }
+}
